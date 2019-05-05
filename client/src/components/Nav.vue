f908a845c6343d1a7894e99bd9763d4f883ea20b
nikita <nikitoz18@gmail.com>
2019-05-05 19:20:56 +0300

    fin

--- client/src/components/Nav.vue
+++ client/src/components/Nav.vue
@@ -7,8 +7,9 @@
 
     <b-collapse id="nav-collapse" is-nav>
       <b-navbar-nav>
-        <b-nav-item href="/">Editor</b-nav-item>
-        <b-nav-item href="/list">Saved documents</b-nav-item>
+        <b-nav-item href="/">Create new markdown</b-nav-item>
+        <b-nav-item href="/list">Edit saved documents</b-nav-item>
+        <b-nav-item href="/preview">Preview</b-nav-item>
       </b-navbar-nav>
     </b-collapse>
   </b-navbar>
