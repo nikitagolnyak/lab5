30639486ee92781eb2e21faf9def75a2c96ae521
nikita <nikitoz18@gmail.com>
2019-05-05 19:16:57 +0300

    delete+preview

---
+++ client/src/components/Nav.vue
@@ -0,0 +1,24 @@
+/* eslint-disable */
+<template>
+  <b-navbar toggleable="lg" type="dark" variant="info">
+    <b-navbar-brand href="#">Markdown editor</b-navbar-brand>
+
+    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+    <b-collapse id="nav-collapse" is-nav>
+      <b-navbar-nav>
+        <b-nav-item href="/">Editor</b-nav-item>
+        <b-nav-item href="/list">Saved documents</b-nav-item>
+      </b-navbar-nav>
+    </b-collapse>
+  </b-navbar>
+</template>
+
+<script>
+  export default {
+    name: "Nav"
+  }
+</script>
+
+<style scoped>
+</style>
